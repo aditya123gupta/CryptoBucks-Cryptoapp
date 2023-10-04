67ddaf48bfb403908744ddc489ea1058e97f87f7
Aditya Gupta <adityagupta11032003@gmail.com>
2023-10-04 23:00:49 +0530

    First Commit

---
+++ src/pages/Crypto.js
@@ -0,0 +1,19 @@
+// Filter and Table Component
+
+
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Filters from "../components/Filters";
+import TableComponent from "../components/TableComponent";
+
+const Crypto = () => {
+  return (
+    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+      <Filters />
+      <TableComponent />
+      <Outlet />
+    </section>
+  );
+};
+
+export default Crypto;
